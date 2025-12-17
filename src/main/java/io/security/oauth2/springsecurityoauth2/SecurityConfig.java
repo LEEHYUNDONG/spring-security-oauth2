e8021b222796f8ad2e2ef214cda6e2ce4a5de14e
LEEHYUNDONG <l.steve10@yahoo.com>
2025-12-17 16:30:15 +0900

    feat: add custom configurer

---
+++ src/main/java/io/security/oauth2/springsecurityoauth2/SecurityConfig.java
@@ -0,0 +1,24 @@
+package io.security.oauth2.springsecurityoauth2;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests().anyRequest().authenticated();
+        http.formLogin();
+        http.apply(new CustomSecurityConfigurer().setIsSecure(false));
+
+
+        return http.build();
+    }
+}
